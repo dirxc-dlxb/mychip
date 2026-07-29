71dcd9b267471b6db2041766e0a80a9357a8938b
dirxc-dlxb <dirxc1902@gmail.com>
2026-07-29 19:02:52 +0900

    Fix published remote-config.js

--- remote-config.js
+++ remote-config.js
@@ -1,10 +1,6 @@
-Exit code: 0
-Wall time: 1.4 seconds
-Output:
 // This public value is safe in a browser. Do not put a service-role key here.
 // Do not put a service-role key in this file.
 window.MYCHIP_REMOTE_CONFIG = {
   apiUrl: 'https://wexyfbfjbiylozxifsft.supabase.co/functions/v1/mychip-api',
   publishableKey: 'sb_publishable_Qt1ODUoCWtWEXUqRuGgJdw_WIeWkF98',
 };
-
